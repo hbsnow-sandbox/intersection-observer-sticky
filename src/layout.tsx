200b306eae759917a4ebaaaeceb5f5d9c2bec1fd
hbsnow <yuki.4uing@gmail.com>
2023-06-10 03:52:33 +0900

    feat: title

--- src/layout.tsx
+++ src/layout.tsx
@@ -8,22 +8,24 @@ export function Layout() {
         <ol className={styles.ol}>
           <li>
             <Link to="/example-01">
-              IntersectionObserver + CircularProgress + sticky
+              IntersectionObserver + CircularProgress + sticky (発生する)
             </Link>
           </li>
           <li>
-            <Link to="/example-02">IntersectionObserver + sticky</Link>
+            <Link to="/example-02">
+              IntersectionObserver + sticky (発生しない)
+            </Link>
           </li>
           <li>
             <Link to="/example-03">
-              IntersectionObserver + LinearProgress + sticky
+              IntersectionObserver + LinearProgress + sticky (発生しない)
             </Link>
           </li>
           <li>
-            <Link to="/example-04">CircularProgress + sticky</Link>
+            <Link to="/example-04">CircularProgress + sticky (発生する)</Link>
           </li>
           <li>
-            <Link to="/example-05">CSS animation + sticky</Link>
+            <Link to="/example-05">CSS animation + sticky (発生する)</Link>
           </li>
         </ol>
       </nav>
